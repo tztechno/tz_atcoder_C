5349a4804053a078ea83611cffa036bf26d569e1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-09 17:33:51 +0900

    Update unique.c

--- unique.c
+++ unique.c
@@ -2,28 +2,21 @@
 #include <string.h>
 
 int main() {
-    char S[1000];
-
+    char S[10];
     if (scanf("%s", S) != 1) {
         fprintf(stderr, "Error reading input\n");
         return 1;
     }
-
-    int count[26] = {0}; // Assuming only lowercase letters, adjust the size accordingly
-
+    int count[26] = {0}; // Assuming only lowercase letters
     for (int i = 0; i < strlen(S); i++) {
-        if ('a' <= S[i] && S[i] <= 'z') {
-            count[S[i] - 'a']++;
-        }
+        count[S[i] - 'a']++;
     }
-
     for (int i = 0; i < strlen(S); i++) {
-        if ('a' <= S[i] && S[i] <= 'z' && count[S[i] - 'a'] == 1) {
+        if (count[S[i] - 'a'] == 1) {
             printf("%c\n", S[i]);
             return 0;
         }
     }
-
     printf("-1\n");
     return 0;
 }
