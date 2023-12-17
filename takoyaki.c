92a319b6295ca9d1c75a01c44dd72f88134eefe2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-17 16:30:51 +0900

    Update takoyaki.c

--- takoyaki.c
+++ takoyaki.c
@@ -3,14 +3,11 @@
 int main() {
     int N, X, T;
     scanf("%d %d %d", &N, &X, &T);
-
-    int t = (N + X - 1) / X; 
-
+    int t = N / X ; 
     if (N % X == 0) {
         printf("%d\n", t * T);
     } else {
         printf("%d\n", (t + 1) * T);
     }
-
     return 0;
 }
