fdc2b0894b98c38a80f7ddc773f859b3035e7e2b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-17 16:23:25 +0900

    Create takoyaki.c

---
+++ takoyaki.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N, X, T;
+    scanf("%d %d %d", &N, &X, &T);
+
+    int t = (N + X - 1) / X; 
+
+    if (N % X == 0) {
+        printf("%d\n", t * T);
+    } else {
+        printf("%d\n", (t + 1) * T);
+    }
+
+    return 0;
+}
