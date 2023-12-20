4a6bd1ff4a2e7de434b349ff530a5cebbfc3696d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-20 15:11:29 +0900

    Create follow.c

---
+++ follow.c
@@ -0,0 +1,15 @@
+//ABC182_A
+//Follow
+//input= 200 300
+
+#include <stdio.h>
+
+int main() {
+  
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int XB=2*A+100;
+    printf("%d\n", XB-B);
+
+    return 0;
+}
