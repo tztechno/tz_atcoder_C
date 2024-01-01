bcd397ef5c56f82ff842f6dcdf21c6626133d806
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-01 15:09:24 +0900

    Update not.c

--- not.c
+++ not.c
@@ -1,16 +1,31 @@
 //ABC178_A Not
 
+######################################
 
 #include <stdio.h>
 
+int main() {
+   int x;
+   scanf("%d", &x);
+   if (x == 0) {
+      printf("1\n");
+   } else if(x ==1) {
+      printf("0\n");
+   } 
+   return 0;
+}
+
+######################################
+
+#include <stdio.h>
 int main() {
     int N;
     scanf("%d", &N);
     if (N==0) {
         printf("%d\n", 1);
-        
     } else {
          printf("%d\n", 0);       
     }
     return 0;
 }
+######################################
