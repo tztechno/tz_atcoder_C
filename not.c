ca3a16a3edc4ba3d646e99e3928f61c29aa11449
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-01 15:07:17 +0900

    Create not.c

---
+++ not.c
@@ -0,0 +1,16 @@
+//ABC178_A Not
+
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    if (N==0) {
+        printf("%d\n", 1);
+        
+    } else {
+         printf("%d\n", 0);       
+    }
+    return 0;
+}
